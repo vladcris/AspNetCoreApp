05737ac39e3183931c019992e78eb1707c758169
vladcris <constantinivlad@yahoo.com>
2022-12-09 14:14:07 +0200

    Routing in angular

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+
+  canActivate(): Observable<boolean> {
+    return this.accountService.currentUser$.pipe(
+      map(user => {
+        if(user) {
+          return true;
+        }
+        else{
+          this.toastr.error('You shall not pass');
+          return false;
+        }
+      })
+    )
+  }
+  
+}
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -13,7 +15,8 @@ export class NavComponent implements OnInit {
   model: any = {};
   //currentUser$: Observable<User | null> = of(null);
 
-  constructor(public accountService: AccountService) { } // dependency injection
+  constructor(public accountService: AccountService, private router: Router,
+              private toastr: ToastrService) { } // dependency injection
 
   ngOnInit(): void {
     //this.currentUser$ = this.accountService.currentUser$;
@@ -25,13 +28,18 @@ export class NavComponent implements OnInit {
       next: response => {
         console.log(response);
       },
-      error: error => console.log(error)
+      error: error => {
+        console.log(error);
+        this.toastr.error(error.error);
+      }
+      
     });
   }
 
   logout()
   {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
